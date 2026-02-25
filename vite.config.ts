80cad6307fde0a23e8ae902673c5c5dc03576d41
Andre Merchant <andremerchant@gmail.com>
2026-02-25 12:34:54 -0600

    solving target src issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://www.andremerchant.cv/',
+  // Esta línea ayuda a resolver importaciones
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
 })
